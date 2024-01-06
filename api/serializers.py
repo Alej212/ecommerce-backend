60dc2719c3d5f85e6d2904eb39afe85f13817914
Alej <alejandrocevedo09092002@gmail.com>
2024-01-05 22:59:15 -0400

    add directory media

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Shoes
 
 class ShoeSerializer(serializers.ModelSerializer):
+    img = serializers.SerializerMethodField()
+
     class Meta:
         model = Shoes
         fields = ['img', 'title', 'money']
+
+    def get_img(self, obj):
+        request = self.context.get('request')
+        if request is not None:
+            return request.build_absolute_uri(obj.img.url)
+        return obj.img.url
