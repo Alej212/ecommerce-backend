565c2d7ad4a55dd907853ce762566d3a04c4a639
Alej <alejandrocevedo09092002@gmail.com>
2024-01-23 17:15:09 -0400

    add more data in models

--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,10 @@ from rest_framework import serializers
 from .models import Products, Shoes, Sweater, Jacket, Pants
 
 class ProductsSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source='_id')
     class Meta:
         model = Products
-        fields = ['image', 'title', 'price', 'category']
+        fields = ['id','image', 'title', 'price', 'category', 'type_product']
 
 class ShoesSerializer(ProductsSerializer):
     class Meta(ProductsSerializer.Meta):
